f1567b957b0ac4845bde0ed3661555bf7c256e3c
danielturcios <danielturciosa@gmail.com>
2021-10-25 15:36:53 -0700

    ex 3.6 - error handling in case of missing name or number

--- index.js
+++ index.js
@@ -80,6 +80,12 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing!'
+        })
+    } 
+
     const person = {
         id: generateId(),
         name: body.name,

b694bd2aa49891d5a5d0f9a5f30302003516cc3c
danielturcios <danielturciosa@gmail.com>
2021-10-25 15:28:39 -0700

    ex 3.5 - new entries in can be added to phonebook with a ID that is randomly generated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         "id": 1,
@@ -61,6 +63,31 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+// client requesting to add a new entry to phonebook
+const generateId = () => {
+    // generates a random number between 1 and 100,000
+    let min = 1
+    let max = 100000
+    
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
